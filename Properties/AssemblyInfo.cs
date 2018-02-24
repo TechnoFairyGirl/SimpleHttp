120ea53f0d87962066d8ea769f35cb787edbcc00
Lily Finley <>
2018-02-24 04:12:11 -0600

    Add Visual Studio project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleHttp")]
+[assembly: AssemblyProduct("SimpleHttp")]
+[assembly: AssemblyDescription("SimpleHTTP allows you to implement HTTP / REST services in .NET applications.")]
+[assembly: AssemblyCompany("Lily Finley")]
+[assembly: AssemblyCopyright("Copyright © Lily Finley 2018")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("20e72952-a1f9-45a7-8910-1fcaf4437493")]
